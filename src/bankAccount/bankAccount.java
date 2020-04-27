78c89e6820bc557b2d3c6edfe8e9bf2ebe9df3d3
Allison Snipes <ajsnipes@usfca.edu>
2020-04-27 10:03:52 -0700

    basic app logic of quiting works

--- src/bankAccount/bankAccount.java
+++ src/bankAccount/bankAccount.java
@@ -7,7 +7,15 @@ public class bankAccount {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); //to get user input
 		int userChoice; // the userChoice is declared as an int
-		boolean quit = false; //
+		boolean quit = false; // the default of quitting the app is set to false
+		do { //make a do while loop for if the user wants to quit
+			System.out.println("Select 0 to exit. "); // prints the instructions to the screen
+			userChoice = in.nextInt(); //takes user input as an int and throws an error if is isnt
+			if (userChoice == 0) {
+				quit = true; // if user selects 0 quit the program
+			}
+		}
+		while(!quit);
 
 	}
 
