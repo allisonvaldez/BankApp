2d01d05aba1c28c6861f597ca3b8e5644384f19d
Allison Snipes <ajsnipes@usfca.edu>
2020-04-27 09:44:03 -0700

    setting up the main class

--- src/bankAccount/bankAccount.java
+++ src/bankAccount/bankAccount.java
@@ -1,9 +1,13 @@
 package bankAccount;
 
+import java.util.Scanner;
+
 public class bankAccount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in); //to get user input
+		int userChoice; // the userChoice is declared as an int
+		boolean quit = false; //
 
 	}
 
